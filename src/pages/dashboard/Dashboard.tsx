af6584d126f1abcb9e3839d59c2f294c33a69635
fabioarilete <fabioarilete@hotmail.com>
2025-03-20 16:07:45 -0300

    feat: barra de ferramentas da listagem

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,9 +1,10 @@
+import { BarraDeFerramentas } from '../../shared/components';
 import { LayoutBaseDePagina } from '../../shared/layouts';
 
 export const Dashboard = () => {
   return (
-    <LayoutBaseDePagina titulo="Página inicial" barraDeFerramentas={<>Barra de Ferramentas</>}>
-      teste
+    <LayoutBaseDePagina titulo="Página inicial" barraDeFerramentas={<BarraDeFerramentas mostrarInputBusca />}>
+      testando
     </LayoutBaseDePagina>
   );
 };
